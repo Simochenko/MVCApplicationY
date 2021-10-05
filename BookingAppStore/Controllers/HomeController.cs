db20f6d5322182e7117e003949baa0d6b73eb553
Serhii Simochenko <s.simochenko@outlook.com>
2021-10-05 19:07:11 +0300

    book store

---
+++ BookingAppStore/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using BookingAppStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookingAppStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        BookContext db = new BookContext();
+
+        public ActionResult Index()
+        {
+            var books = db.Books;
+            ViewBag.Books = books;
+            return View();
+        }
+
+
+
+      /*  public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }*/
+    }
+}
