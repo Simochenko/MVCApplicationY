494405ef92548b6d3ac89137902037b43f662314
Serhii Simochenko <s.simochenko@outlook.com>
2021-10-22 12:47:18 +0300

    CRUD

--- NavigationApp/Controllers/HomeController.cs
+++ NavigationApp/Controllers/HomeController.cs
@@ -17,6 +17,66 @@ namespace NavigationApp.Controllers
             return View(players.ToList());
         }
 
+        [HttpGet]
+        public ActionResult Create()
+        {
+            //формируем список команд для передачи в представление
+            SelectList teams = new SelectList(db.Teams, "Id", "Name");
+            ViewBag.Teams = teams;
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(Player player)
+        {
+            //Добавляем игрока в таблицу
+            db.Players.Add(player);
+            db.SaveChanges();
+            //перенаправляем на главную страницу
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            if (id ==null)
+            {
+                return HttpNotFound();
+            }
+            //Находим в бд футболиста
+            Player player = db.Players.Find(id);
+            if(player != null)
+            {
+                // Создаём список команд для передачи вв представление
+                SelectList teams = new SelectList(db.Teams, "Id", " Name", player.TeamId);
+                ViewBag.Teams = teams;
+                return View(player);
+            }
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Player player)
+        {
+            db.Entry(player).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return HttpNotFound();
+            }
+            // Находим в бд футболиста
+            Player player = db.Players.Find(id);
+            if (player != null)
+            {
+                db.Players.Remove(player);
+                db.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
