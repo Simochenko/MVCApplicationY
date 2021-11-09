485109567d881f262d67b07e0836ec617b7c533b
Serhii Simochenko <s.simochenko@outlook.com>
2021-11-09 13:22:11 +0200

    CustomIdentity,Identity,Routes

---
+++ CustomIdentityApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CustomIdentityApp.MvcApplication" Language="C#" %>
---
+++ IdentityApp/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using IdentityApp.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace IdentityApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult GetMyRoles()
+        {
+            IList<string> roles = new List<string> { "Роль не определена" };
+            ApplicationUserManager userManager = HttpContext.GetOwinContext()
+                                                    .GetUserManager<ApplicationUserManager>();
+            ApplicationUser user = userManager.FindByEmail(User.Identity.Name);
+            if (user != null)
+                roles = userManager.GetRoles(user.Id);
+            return View(roles);
+        }
+
+        public ActionResult GetUser()
+        {
+            List<ApplicationUser> users = new List<ApplicationUser>();
+            using(ApplicationDbContext db = new ApplicationDbContext())
+            {
+                users = db.Users.ToList();
+            }
+            return View(users);
+        }
+        [Authorize(Roles = "admin")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
